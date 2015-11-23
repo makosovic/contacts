35f191664f923b777f9bf6e2d8177bc4487adabb
Marko Kosović <kosovic.marko@hotmail.com>
2015-11-23 21:02:45 +0000

    Added bower.

--- Contacts.Web/App_Start/BundleConfig.cs
+++ Contacts.Web/App_Start/BundleConfig.cs
@@ -9,9 +9,16 @@ namespace Contacts.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
-                ));
+                "~/bower_components/angular/angular.js",
+                "~/bower_components/angular-route/angular-route.js",
+                "~/bower_components/angular-animate/angular-animate.js",
+                "~/bower_components/angular-aria/angular-aria.js",
+                "~/bower_components/angular-material/angular-material.js",
+                "~/bower_components/angular-messages/angular-messages.js",
+                "~/bower_components/ng-breadcrumbs/ng-breadcrumbs.js"));
 
             bundles.Add(new StyleBundle("~/bundles/styles").Include(
+                "~/bower_components/angular-material/angular-material.css",
                 "~/Content/site.css"));
         }
     }
