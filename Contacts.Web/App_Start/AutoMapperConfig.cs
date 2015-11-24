81ef3dd34b1dce037deb21c1ee4394460f34bbdc
Marko Kosović <kosovic.marko@hotmail.com>
2015-11-24 12:49:12 +0000

    Register initial maps to automapper config.

---
+++ Contacts.Web/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Contacts.Web.Entities;
+using Contacts.Web.Models.Contact;
+using Contacts.Web.Models.ContactInfo;
+
+namespace Contacts.Web
+{
+    public static class AutoMapperConfig
+    {
+        public static void RegisterMappings()
+        {
+            AutoMapper.Mapper.CreateMap<Contact, ContactListModel>();
+
+            AutoMapper.Mapper.CreateMap<Contact, ContactEditModel>()
+                .ForMember(dest => dest.Tags,
+                           opts => opts.MapFrom(src => src.Tags.Select(x => x.Name)));
+
+            AutoMapper.Mapper.CreateMap<ContactInfo, ContactInfoEditModel>()
+                .ForMember(dest => dest.Type,
+                           opts => opts.MapFrom(src => src.Type.ToString()));
+        }
+    }
+}
